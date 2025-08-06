f7ac9c2669f615b2e3c526e83fa47626fc6cc5b5
Suguru Yamamoto <sgryjp@gmail.com>
2025-08-06 16:02:06 +0900

    refactor: avoid unnecessarily complex format logic

--- cjkfmt-cli/src/cli/utils.rs
+++ cjkfmt-cli/src/cli/utils.rs
@@ -6,21 +6,13 @@ pub fn format_diagnostic(diagnostic: &Diagnostic) -> String {
     let filename = diagnostic.filename.as_deref().unwrap_or("<stdin>");
     let line = diagnostic.start.line;
     let column = diagnostic.start.column;
-    let buf = format!(
-        "{}\0{}\0{}\0{}\0{}",
-        filename,
-        line + 1,
-        column + 1,
-        diagnostic.code,
-        diagnostic.message
-    );
-    let tokens: Vec<&str> = buf.split('\0').collect();
+
     let colon = ":".cyan();
-    let filename = tokens[0].white().bold();
-    let line = tokens[1];
-    let column = tokens[2];
-    let code = tokens[3].yellow();
-    let message = tokens[4];
+    let filename = filename.white().bold();
+    let line = (line + 1).to_string();
+    let column = (column + 1).to_string();
+    let code = diagnostic.code.yellow();
+    let message = &diagnostic.message;
 
     format!(
         "{filename}{}{line}{}{column}{} {code} {message}",
